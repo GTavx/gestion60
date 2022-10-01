3be03690f259c569b393b3063e669d78ec394a87
GTavx <tavalmuro@gmail.com>
2022-10-01 18:49:23 -0500

    archivos para deploy

--- src/main/java/udea/edu/co/sistemagestion/gestion/Controladores/MainController.java
+++ src/main/java/udea/edu/co/sistemagestion/gestion/Controladores/MainController.java
@@ -1,18 +1,9 @@
 package udea.edu.co.sistemagestion.gestion.Controladores;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import udea.edu.co.sistemagestion.gestion.Entidades.Employee;
-import udea.edu.co.sistemagestion.gestion.Servicios.ServicesEmployee;
-import udea.edu.co.sistemagestion.gestion.Servicios.ServicesEnterprise;
-
-import java.util.List;
-
-
 @Controller
 public class MainController {
    @GetMapping("/")
